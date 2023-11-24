ee963d70a4fa776c01e12173e76c528c3fcbeabc
DominicOcharo1 <149952803+DominicOcharo1@users.noreply.github.com>
2023-11-24 18:31:11 +0300

    Update 1_🏠_Detection.py

--- 1_🏠_Detection.py
+++ 1_🏠_Detection.py
@@ -94,6 +94,35 @@ with st.container():
         # Add other source types (video, webcam, youtube) and their corresponding functions here
 
     elif model_task == 'Classification':
+        model = tf.keras.models.load_model("weights/maize_class.hdf5")
+        ### load file
+        uploaded_file = st.file_uploader("Choose a image file", type="jpg")
+          
+        map_dict = {0: 'fall_armyworm',
+                    1: 'grasshopper',
+                    2: 'leaf_blight',
+                    3: 'leaf_spot'}
+        
+        
+        if uploaded_file is not None:
+            # Convert the file to an opencv image.
+            file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+            opencv_image = cv2.imdecode(file_bytes, 1)
+            opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+            resized = cv2.resize(opencv_image,(224,224))
+            # Now do something with the image! For example, let's display it:
+            st.image(opencv_image, channels="RGB")
+        
+            resized = mobilenet_v2_preprocess_input(resized)
+            img_reshape = resized[np.newaxis,...]
+        
+            Genrate_pred = st.button("Generate Prediction")    
+            if Genrate_pred:
+                prediction = model.predict(img_reshape).argmax()
+                st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
+
+        
+       '''
         def predict_class(image_path, model):
             # Load and preprocess the image
             img = Image.open(image_path)
@@ -206,3 +235,4 @@ with st.container():
                             """)
             else:
                 st.write("Please upload an image before pressing the Predict button.")
+'''
