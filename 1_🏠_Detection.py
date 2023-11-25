d81972b10b75d937cc4894c02bd598bdd7f04368
DominicOcharo1 <149952803+DominicOcharo1@users.noreply.github.com>
2023-11-25 23:06:33 +0300

    Update 1_🏠_Detection.py

--- 1_🏠_Detection.py
+++ 1_🏠_Detection.py
@@ -117,7 +117,7 @@ with st.container():
         
         if uploaded_file is not None:
             col1, col2 = st.columns(2)
-            with col2:
+            with col1:
                 # Convert the file to an opencv image.
                 file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
                 opencv_image = cv2.imdecode(file_bytes, 1)
